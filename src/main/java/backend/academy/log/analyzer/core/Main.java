c19aa084f7aa48e0f78c09cbc9ec9688376b9bfb
Ivan Kabernyuk <kabernyuk.dev@gmail.com>
2024-11-08 18:13:29 +0300

    feat: create reader from path parametr

--- src/main/java/backend/academy/log/analyzer/core/Main.java
+++ src/main/java/backend/academy/log/analyzer/core/Main.java
@@ -1,8 +1,13 @@
 package backend.academy.log.analyzer.core;
 
+import backend.academy.log.analyzer.read.LogReader;
+import backend.academy.log.analyzer.read.LogReaderFactory;
+
 public class Main {
     public static void main(String[] args) {
-        ArgumentParser parser = new ArgumentParser();
-        parser.initialize(args);
+        ArgumentParser argumentParser = new ArgumentParser();
+        argumentParser.initialize(args);
+
+        LogReader logReader = LogReaderFactory.create(argumentParser.path());
     }
 }

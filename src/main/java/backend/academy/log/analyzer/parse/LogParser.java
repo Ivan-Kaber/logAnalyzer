d26d26d6481d22f2b2a1137e2601f65e1cdfad78
Ivan Kabernyuk <kabernyuk.dev@gmail.com>
2024-11-08 17:47:21 +0300

    feat: Add LogParser class

---
+++ src/main/java/backend/academy/log/analyzer/parse/LogParser.java
@@ -0,0 +1,9 @@
+package backend.academy.log.analyzer.parse;
+
+import backend.academy.log.analyzer.model.LogInformation;
+
+public class LogParser {
+    public static LogInformation parseLine(String line) {
+        return null;
+    }
+}

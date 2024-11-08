bc78a7f353be856b804e1c7b212ee1bd7a526872
Ivan Kabernyuk <kabernyuk.dev@gmail.com>
2024-11-08 18:23:25 +0300

    feat: Add GenerateReportException

---
+++ src/main/java/backend/academy/log/analyzer/exception/GenerateReportException.java
@@ -0,0 +1,7 @@
+package backend.academy.log.analyzer.exception;
+
+public class GenerateReportException extends RuntimeException {
+    public GenerateReportException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}

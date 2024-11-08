19e45b29049927a343b62a4c3ea7a176f3132918
Ivan Kabernyuk <kabernyuk.dev@gmail.com>
2024-11-08 16:26:14 +0300

    feat: Add common interface for file and url readers

---
+++ src/main/java/backend/academy/log/analyzer/read/LogReader.java
@@ -0,0 +1,9 @@
+package backend.academy.log.analyzer.read;
+
+import backend.academy.log.analyzer.model.LogInformation;
+import java.util.List;
+import java.util.stream.Stream;
+
+public interface LogReader {
+    Stream<LogInformation> readLogs();
+}
